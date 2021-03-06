d77c91f08f90b166c387ed540a51e95e587b2b8d
arifanf <79965177+arifanf@users.noreply.github.com>
2021-03-06 17:40:16 +0700

    Pertemuan1
    PBO 4417

---
+++ Pertemuan1/StringDemo.java
@@ -0,0 +1,17 @@
+// public class StringDemo {
+
+//    public static void main(String args[]) {
+//       char[] helloArray = { 'h', 'e', 'l', 'l', 'o', '.' };
+//       String helloString = new String(helloArray);  
+//       System.out.println( helloString );
+//    }
+// }
+
+public class StringDemo {
+
+    public static void main(String args[]) {
+       String palindrome = "Dot saw I was Tod"; // dihitung per karakter n space
+       int len = palindrome.length();
+       System.out.println( "String Length is : " + len );
+    }
+ }
---
+++ Pertemuan1/Test.java
@@ -0,0 +1,44 @@
+// public class Test {
+//    public void pupAge() {
+//       int age = 0;
+//       age = age + 7;
+//       System.out.println("Puppy age is : " + age);
+//    }
+
+//    public static void main(String args[]) {
+//       Test test = new Test();
+//       test.pupAge();
+//    }
+// }
+
+// public class Test {
+
+//    public static void main(String args[]) {
+//       int a, b;
+//       a = 10;
+//       b = (a == 1) ? 20: 30;
+//       System.out.println( "Value of b is : " +  b );
+
+//       b = (a == 10) ? 20: 30;
+//       System.out.println( "Value of b is : " + b );
+//    }
+// }
+
+public class Test {
+
+   public static void main(String args[]) {
+      int [] numbers = {10, 20, 30, 40, 50};
+
+      for(int x : numbers ) {
+         System.out.print( x );
+         System.out.print(",");
+      }
+      System.out.print("\n");
+      String [] names = {"James", "Larry", "Tom", "Lacy"};
+
+      for( String name : names ) {
+         System.out.print( name );
+         System.out.print(",");
+      }
+   }
+}

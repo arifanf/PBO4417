8fe4a096f20957efdcd2933fa661076590ee21d5
arifanf <79965177+arifanf@users.noreply.github.com>
2021-05-11 20:52:15 +0700

    Praktikum 8
    Pertemuan 8 - Kasus 1 - Sistem Informasi Akademik Sederhana

---
+++ Pertemuan8/Praktik/src/SistemAkademik/SistemAkademik.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SistemAkademik;
+
+/**
+ *
+ * @author ASUS
+ */
+interface SistemAkademik {
+    public int getNoId();
+    public void setNoId();
+    public void setNoId(int noId);
+    public void setData();
+    public void show();
+}
